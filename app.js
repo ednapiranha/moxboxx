b3ef372e81b7987918cd8a057a416e48484ed578
Jen Fong-Adwent <jen@ednapiranha.com>
2012-12-02 19:37:09 -0500

    removed temp

--- app.js
+++ app.js
@@ -5,37 +5,6 @@ var server = require('http').createServer(app);
 var settings = require('./settings')(app, configurations, express);
 var nconf = require('nconf');
 
-/** TEMP **/
-var redis = require('redis');
-var client = redis.createClient();
-var playl = require('./lib/playlist');
-
-client.lrange('moxboxx:global:playlists:list', 0, -1, function(err, p) {
-  p.forEach(function(pkey) {
-    client.hgetall('moxboxx:playlist:hash:' + pkey, function(err, pl) {
-      if (pl) {
-        var req = {
-          session: {
-            userId: pl.owner
-          },
-          body: {}
-        };
-        req.body.title = pl.title;
-        req.body.created = pl.created;
-
-        playl.add(req, function(err, user) {
-          if (err) {
-            console.error('could not save', err)
-          } else {
-            console.log('saved')
-          }
-        });
-      }
-    });
-  });
-});
-/** END TEMP **/
-
 nconf.argv().env().file({ file: 'local.json' });
 
 /* Filters for routes */
